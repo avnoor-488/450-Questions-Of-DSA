86dad2cad9f49c02ee27fae77daddcf05475256a
Avnoor <54592053+avnoor-488@users.noreply.github.com>
2021-07-12 05:29:52 +0530

    Create Rotate_90_Degree.cpp

---
+++ Rotate_90_Degree.cpp
@@ -0,0 +1,99 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define forT(t) for(int i=0;i<t;i++)
+#define forN(n) for(int j=0;j<n;j++)
+#define MOD 1000000007
+#define num 4e6 + 5
+
+
+
+typedef pair<int, int> pii;
+typedef vector<string> vs;
+typedef set<int> seti;
+typedef long long int ll;
+typedef vector<int> vi;
+typedef vector<vector<int>> vv;
+
+
+void fastscan( ll &number)
+{
+    bool negative = false;
+    register int c;
+
+    number = 0;
+
+    c = getchar();
+    if (c == '-')
+    {
+        negative = true;
+
+        c = getchar();
+    }
+
+    for (; (c > 47 && c < 58); c = getchar())
+        number = number * 10 + c - 48;
+
+
+    if (negative)
+        number *= -1;
+}
+
+ll gcd (ll a, ll b)
+{
+    if (b == 0)
+        return a;
+
+    return gcd(b, a % b);
+}
+
+ll dp[100010][110][2];
+ll arr[100010];
+
+void solve(vector<vector<int> > arr, int row, int col)
+{
+    int sCol = 0;
+    // cout << "solve";
+    while (sCol != col )
+    {
+        for (int i = row - 1 ; i >= 0; i--)
+        {
+            cout << arr[i][sCol] << " ";
+
+        }
+        cout << endl;
+        sCol++;
+    }
+}
+
+int main() {
+
+// #ifndef ONLINE_JUDGE
+//     freopen("input.txt", "r", stdin);
+//     freopen("output.txt", "w", stdout);
+// #endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int row, col;
+    cin >> row >> col;
+    vector<vector<int> > matrix(row);
+    forT(row)
+    {
+        matrix[i].assign(col, 0);
+        forN(col)
+        {
+
+            cin >> matrix[i][j];
+        }
+    }
+    solve(matrix, row, col);
+    return 0;
+}
+
+// coded with the ❤️ Avnoor
+
+
