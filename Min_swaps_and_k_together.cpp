9ca9d296c63d066adcf7eb2cb3dfcc5902ae59b0
Avnoor <54592053+avnoor-488@users.noreply.github.com>
2021-07-11 17:24:52 +0530

    Create Min_swaps_and_k_together.cpp

---
+++ Min_swaps_and_k_together.cpp
@@ -0,0 +1,117 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define forT(t) for(int i=0;i<t;i++)
+#define forN(n) for(int j=0;j<n;j++)
+#define MOD 1000000007
+#define num 4e6 + 5
+
+
+
+typedef pair<int, int> pii;
+typedef vector<string> vs;
+typedef set<int> seti;
+typedef long long int ll;
+typedef vector<int> vi;
+typedef vector<vector<int>> vv;
+
+
+void fastscan( ll &number)
+{
+    bool negative = false;
+    register int c;
+
+    number = 0;
+
+    c = getchar();
+    if (c == '-')
+    {
+        negative = true;
+
+        c = getchar();
+    }
+
+    for (; (c > 47 && c < 58); c = getchar())
+        number = number * 10 + c - 48;
+
+
+    if (negative)
+        number *= -1;
+}
+
+ll gcd (ll a, ll b)
+{
+    if (b == 0)
+        return a;
+
+    return gcd(b, a % b);
+}
+
+ll dp[100010][110][2];
+ll arr[100010];
+
+int solve(vi arr, int key)
+{
+
+    int n = arr.size(), dec = 0, count = 0;
+
+    forT(n)
+    {
+        if (arr[i] <= key)count++;
+        else continue;
+    }
+
+    forT(count)
+    {
+        if (arr[i] > key)dec++;
+        else continue;
+    }
+
+    int ans = dec;
+
+    for (int i = 0, j = count; j < n; i++, j++)
+    {
+        if (arr[i] > key)dec--;
+        if (arr[j] > key)dec++;
+
+        ans = min(dec, ans);
+    }
+
+    return ans;
+
+}
+
+int main() {
+
+// #ifndef ONLINE_JUDGE
+//     freopen("input.txt", "r", stdin);
+//     freopen("output.txt", "w", stdout);
+// #endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    cin >> n;
+    vi final;
+
+    forN(n)
+    {
+        int no;
+        cin >> no;
+        final.pb(no);
+
+    }
+
+    int key;
+    cin >> key;
+
+    cout << solve(final, key);
+    return 0;
+}
+
+// coded with the ❤️ Avnoor
+
+
