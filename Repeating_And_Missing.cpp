ceea19e7d88efb2e5bc689d9c9dc19ca6114e62e
Avnoor <54592053+avnoor-488@users.noreply.github.com>
2021-07-13 00:37:26 +0530

    Create Repeating_And_Missing.cpp

---
+++ Repeating_And_Missing.cpp
@@ -0,0 +1,110 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define forT(t) for(int i=0;i<t;i++)
+#define forN(n) for(int j=0;j<n;j++)
+#define MOD 1000000007
+#define num 4e6 + 5
+
+
+
+typedef pair<int, int> pii;
+typedef vector<string> vs;
+typedef set<int> seti;
+typedef long long int ll;
+typedef vector<int> vi;
+typedef vector<vector<int>> vv;
+
+
+void fastscan( ll &number)
+{
+    bool negative = false;
+    register int c;
+
+    number = 0;
+
+    c = getchar();
+    if (c == '-')
+    {
+        negative = true;
+
+        c = getchar();
+    }
+
+    for (; (c > 47 && c < 58); c = getchar())
+        number = number * 10 + c - 48;
+
+
+    if (negative)
+        number *= -1;
+}
+
+ll gcd (ll a, ll b)
+{
+    if (b == 0)
+        return a;
+
+    return gcd(b, a % b);
+}
+
+ll dp[100010][110][2];
+// ll arr[100010];
+
+vi solve(vi vec)
+{
+    int n = vec.size();
+    vi output;
+    int missing = 0, twice = 0;
+    sort(vec.begin(), vec.end());
+
+    for (int i = 0; i < n; i++)
+    {
+        if (vec[i] == i + 1)continue;
+        else missing = i + 1;
+
+        twice ^= vec[i];
+    }
+  
+    output.pb(twice);
+    output.pb(missing);
+    return output;
+}
+
+
+
+int main() {
+
+// #ifndef ONLINE_JUDGE
+//     freopen("input.txt", "r", stdin);
+//     freopen("output.txt", "w", stdout);
+// #endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int row, col;
+    cin >> row ;
+    vi matrix;
+    forT(row)
+    {
+        int no;
+        cin >> no;
+        matrix.pb(no);
+        // cin >> matrix[i];
+
+    }
+
+    vi output = solve(matrix);
+    for (int i = 0; i < output.size(); i++)
+    {
+        cout << output[i] << " ";
+    }
+    
+    return 0;
+}
+
+// coded with the ❤️ Avnoor
+
+
